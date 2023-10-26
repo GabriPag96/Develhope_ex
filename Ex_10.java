86f8bfd610e3ce34e24502eb7350689d867b533e
Gabri <gabripag@outlook.com>
2023-10-26 19:01:25 +0200

    Ex_10 added.

---
+++ Ex_10.java
@@ -0,0 +1,30 @@
+public class Ex_10 {
+    public static void main(String [] args) {
+
+        int num1 = 5;
+        int num2 = 3;
+
+        System.out.println(confronto1(num1, num2));
+
+        System.out.println(confronto2(num1, num2));
+
+        System.out.println(confronto3(num1, num2));
+
+        System.out.println(confronto4(num1, num2));
+    }
+    public static boolean confronto1(int num1, int num2){
+        return num1 != num2;
+    }
+    public static boolean confronto2(int num1, int num2){
+        return num1 > num2;
+    }
+    public static boolean confronto3(int num1, int num2){
+        return num1 < num2;
+    }
+
+    public static boolean confronto4(int num1, int num2){
+        return num1 == num2;
+    }
+}
+
+
