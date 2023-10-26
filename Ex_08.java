4e0ed29c076d67562302fa069aa207eae0a96552
Gabri <gabripag@outlook.com>
2023-10-26 18:24:20 +0200

    Ex_08 added.

---
+++ Ex_08.java
@@ -0,0 +1,13 @@
+public class Ex_08 {
+    public static void main(String [] args) {
+
+        double num1 = 24;
+        double num2 = 27;
+        double num3 = 18;
+
+
+
+        System.out.println(calcMedia(num1, num2, num3));
+    }
+    public static double calcMedia(double num1, double num2, double num3){ return (num1+num2+num3)/3; }
+}
