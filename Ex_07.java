69789e74bb08687844b356a4370c663a020aca25
Gabri <gabripag@outlook.com>
2023-10-26 18:18:04 +0200

    Ex_07 added.

---
+++ Ex_07.java
@@ -0,0 +1,9 @@
+public class Ex_07 {
+    public static void main(String[] args){
+        int num1 = 20;
+        int num2 = 4;
+
+        System.out.println("Il quoziente della divisione è: " + divisione(num1, num2));
+    }
+    public static int divisione(int num1, int num2){return num1/num2;}
+}
