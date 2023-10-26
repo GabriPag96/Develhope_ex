da3ead566210216fa190a11ab9180b520367e849
Gabri <gabripag@outlook.com>
2023-10-26 16:35:06 +0200

    Ex_18 added.

--- Ex_18.java
+++ Ex_18.java
@@ -1,4 +1,8 @@
 public class Ex_18 {
+    //Scrivere un programma che contenga un metodo che sfrutti il while loop e prenda in ingresso
+    // 1 valore intero come limite superiore e calcoli la somma di tutti i valori prima del limite e la stampi a video.
+    // (Esempio passo 5 come limite e otterrò la somma di 1 + 2 + 3 + 4)
+
     public static void main(String [] args){
         int valoreMassimo = 5;
         int sommaValore = 0;
