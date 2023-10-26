9710685a4cd4fed3a4c2679a5da60aba0ace615f
Gabri <gabripag@outlook.com>
2023-10-26 18:33:41 +0200

    Ex_09 added.

---
+++ Ex_09.java
@@ -0,0 +1,22 @@
+public class Ex_09 {
+    public static void main(String [] args) {
+
+        int num1 = 10;
+        int num2 = 26;
+
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        System.out.println(incremento(num1, num2));
+    }
+    public static int incremento(int num1, int num2){
+        int incremento = 4;
+        num1 += incremento;
+        num2 += incremento;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        return num1 * num2;
+    }
+}
