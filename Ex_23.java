7d3b6b86066f93d95e468a877898b8d90cb13fd2
Gabri <gabripag@outlook.com>
2023-10-27 16:28:09 +0200

    sistemato es. 23.

--- Ex_23.java
+++ Ex_23.java
@@ -1,6 +1,6 @@
 /*Scrivi un programma che contenga un metodo che crea un array e
-        lo riempie con i numeri da 1 a 10 e che che calcoli la somma
-        dei numeri contenuti nell'array inizializzato e la stampi a video.*/
+lo riempie con i numeri da 1 a 10 e che che calcoli la somma
+dei numeri contenuti nell'array inizializzato e la stampi a video.*/
 public class Ex_23 {
     public static void main(String[] args){
         int[] array = createArray();
