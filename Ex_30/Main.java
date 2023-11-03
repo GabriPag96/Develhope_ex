930e88663a0eed1f353ea9573d5ae2be38f2b11d
Gabri <gabripag@outlook.com>
2023-11-03 10:19:52 +0100

    aggiunto package es. 30.

---
+++ Ex_30/Main.java
@@ -0,0 +1,10 @@
+package Ex_30;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Auto auto1 = new Auto("2000cc", "AB123CD", "Fiat", "Panda");
+
+        System.out.println(auto1);
+    }
+}
