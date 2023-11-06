99e2e7f01a22f12f13c492a69a5642fe0f0d450b
Gabri <gabripag@outlook.com>
2023-11-06 15:47:27 +0100

    aggiunto package es. 32.

---
+++ Ex_32/Main.java
@@ -0,0 +1,9 @@
+package Ex_32;
+
+public class Main {
+    public static void main(String[] args){
+        Rettangolo rettangolo = new Rettangolo(5.0, 10.0);
+
+        rettangolo.calcolaArea();
+    }
+}
