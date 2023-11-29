04721b253862939fcbdaacfffee0d6baf3038141
Gabri <gabripag@outlook.com>
2023-11-29 16:46:18 +0100

    aggiustato package es. 58.

--- Ex_58/Main.java
+++ Ex_58/Main.java
@@ -9,10 +9,14 @@ public class Main {
 
         OffsetDateTime dateExercise2 = OffsetDateTime.parse("2024-03-01T13:00:00Z");
 
+        boolean isDateNow = dateExercise1.isEqual(OffsetDateTime.now());
+
         System.out.println("La prima data è precedente alla seconda? " + dateExercise1.isBefore(dateExercise2));
 
         System.out.println("La seconda data è successiva alla prima? " + dateExercise2.isAfter(dateExercise1));
 
         System.out.println("Le date sono uguali? " + dateExercise1.isEqual(dateExercise2));
+
+        System.out.println("La data no 1 è oggi? " + isDateNow);
     }
 }
